0468b38cc59282fa785943f89ffdb3ab3118a178
Carolina Morales <carolina.cmorales3@gmail.com>
2022-04-11 21:33:05 -0500

    layout component

--- src/components/organism/Layout.tsx
+++ src/components/organism/Layout.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
+import { Container, Theme } from '@mui/material';
+import { makeStyles } from '@mui/styles';
 
-const Layout = () => {
+const useStyles = makeStyles((theme: Theme) => ({
+  container: {
+    padding: 24,
+    height: '100vh',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+}));
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  const classes = useStyles();
+
   return (
-    <div>
-
-    </div>
+    <Container maxWidth='lg' className={classes.container}>
+      {children}
+    </Container>
   );
 };
+
+interface LayoutProps {
+  children: any
+}
 
 export default Layout;
